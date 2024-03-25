a9266a9affea6d374786d5df774253e871be0ee4
O-m0desto <133922393+O-m0desto@users.noreply.github.com>
2024-03-25 20:37:34 -0300

    Exercícios Melhorados

--- Aula06/exercicio3.py
+++ Aula06/exercicio3.py
@@ -3,19 +3,27 @@ largura = float(input("Informe o valor da largura do aposento: "))
 qtdLitros = int(input("Informe a quantidade de tinta em litros: "))
 
 area_porta = 0.80 * 2.10
-area_parede = (comprimento * largura)
-area_para_pintar = ((area_parede*4) - area_porta)
+area_parede_1 = (comprimento * 2.80)
+area_parede_2 = (largura * 2.80)
+area_paredes = 2*(area_parede_1 + area_parede_2)
+area_para_pintar = area_paredes - area_porta
 
 if (area_para_pintar % 3 >= 1):
     qtdLatasNecessarias = (area_para_pintar//3 + 1)
 else:
     qtdLatasNecessarias = area_para_pintar / 3
 
-print(f"A quantidade de litros necessários para pintar o comodo são de {qtdLatasNecessarias/3} litros de tinta")
+if (qtdLatasNecessarias % 3 != 1):
+    qtdReal = (qtdLatasNecessarias//3) + 1
+else:
+    qtdReal = (qtdLatasNecessarias//3)
 
-if(qtdLitros > qtdLatasNecessarias):
+
+print(f"A quantidade de litros necessários para pintar o comodo são de {qtdReal} litros de tinta")
+
+if(qtdLitros > qtdReal):
     print("E você já possui a mais que a quantidade necessária de tinta")
-elif(qtdLitros == qtdLatasNecessarias):
+elif(qtdLitros == qtdReal):
     print("e você possui a quantidade exata necessaria de tinta")
 else:
-    print(f"então você tem de comprar {qtdLatasNecessarias - qtdLitros} litros de tinta")
+    print(f"então você tem de comprar {qtdReal - qtdLitros} litros de tinta")
